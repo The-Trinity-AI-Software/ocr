3fe738ec5619f77db75d9762a409535c9127bb3a
ravikiran2015 <ravi@thetrinitysoftware.com>
2025-05-20 22:05:27 +0530

    Update aws_utils.py

--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -9,11 +9,15 @@ def parse_s3_uri(s3_uri):
         raise ValueError(f"Invalid S3 URI structure: {s3_uri}")
     return parts[0], parts[1]
 
-def download_single_image_from_s3(access_key, secret_key, s3_uri, local_dir):
+def download_single_image_from_s3(s3_uri, local_dir):
+    access_key = "AKIA3TVWE5ELUK6JG2WK"
+    secret_key = "jTL7V8vEq0r5dFR5VNCRkWL66v3yg9t55bDJlLTw"  # Replace with your actual secret key
+
     os.makedirs(local_dir, exist_ok=True)
     bucket, key = parse_s3_uri(s3_uri)
     filename = os.path.basename(key)
     local_path = os.path.join(local_dir, filename)
+
     s3 = boto3.client(
         's3',
         aws_access_key_id=access_key,
@@ -21,3 +25,4 @@ def download_single_image_from_s3(access_key, secret_key, s3_uri, local_dir):
     )
     s3.download_file(bucket, key, local_path)
     return [local_path]
+
